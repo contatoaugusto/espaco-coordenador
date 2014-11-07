40f0e295026df4f4c6ad81b59aa076441a2f1d2d
contatoaugusto <contatoaugusto@gmail.com>
2014-11-07 00:17:35 +0000

    Representante de turma

---
+++ EC.Dado/DAluno.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using EC.Modelo;
+
+namespace EC.Dado
+{
+    public class DAluno
+    {
+        public List<ALUNO> Consultar()
+        {
+            using (ECEntities db = new ECEntities())
+            {
+                var l = db.ALUNO.ToList();
+                List<ALUNO> ltAluno = new List<ALUNO>();
+
+                foreach (var tipo in l)
+                {
+                    ALUNO aluno = new ALUNO();
+                    aluno.ID_ALUNO = tipo.ID_ALUNO;
+                    aluno.RA  = tipo.RA;
+                    aluno.ATIVO = tipo.ATIVO;
+                    aluno.ID_PESSOA = tipo.ID_PESSOA;
+
+                    aluno.PESSOA = db.PESSOA.First(rs => rs.ID_PESSOA == tipo.ID_PESSOA);
+
+                    ltAluno.Add(aluno);
+                }
+
+                return ltAluno;
+            }
+        }
+
+        public ALUNO ConsultarByRA(int nuRA)
+        {
+            using (ECEntities db = new ECEntities())
+            {
+                var aluno = db.ALUNO.First(rs => rs.RA == nuRA);
+                aluno.PESSOA = db.PESSOA.First(rs => rs.ID_PESSOA == aluno.ID_PESSOA);
+                return aluno;
+            }
+        }
+
+        public ALUNO ConsultarById(int idAluno)
+        {
+            using (ECEntities db = new ECEntities())
+            {
+                var aluno = db.ALUNO.First(rs => rs.ID_ALUNO == idAluno);
+                aluno.PESSOA = db.PESSOA.First(rs => rs.ID_PESSOA == aluno.ID_PESSOA);
+                return aluno;
+            }
+        }
+
+        public List<ALUNO> ConsultarByTurma(int idTurma)
+        {
+            using (ECEntities db = new ECEntities())
+            {
+                List<ALUNO> ltAluno = new List<ALUNO>();
+
+                var alunoMatricula = db.ALUNO_MATRICULA.Where(rs => rs.ID_TURMA == idTurma);
+
+                foreach (var am in alunoMatricula)
+                {
+                    var alunos = db.ALUNO.Where(rs => rs.ID_ALUNO == am.ID_ALUNO);
+
+                    foreach (var tipo in alunos)
+                    {
+                        ALUNO aluno = new ALUNO();
+                        aluno.ID_ALUNO = tipo.ID_ALUNO;
+                        aluno.RA = tipo.RA;
+                        aluno.ATIVO = tipo.ATIVO;
+                        aluno.ID_PESSOA = tipo.ID_PESSOA;
+
+                        aluno.PESSOA = db.PESSOA.First(rs => rs.ID_PESSOA == tipo.ID_PESSOA);
+
+                        ltAluno.Add(aluno);
+                    }
+                }
+                return ltAluno;
+            }
+        }
+       
+        public void Salvar(TURMA q)
+        {
+            using (ECEntities db = new ECEntities())
+            {
+                db.TURMA.AddObject(q);
+                db.SaveChanges();
+
+                var id = q.ID_TURMA;
+
+                db.Dispose();
+            }
+        }     
+    }
+}
---
+++ EC.Negocio/NAluno.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EC.Modelo;
+using EC.Dado;
+
+namespace EC.Negocio
+{
+    public class NAluno
+    {
+
+        public static List<ALUNO> Consultar()
+        {
+            return (new DAluno()).Consultar();
+        }
+
+        public static List<ALUNO> ConsultarByTurma(int idTurma)
+        {
+            return (new DAluno()).ConsultarByTurma(idTurma);
+        }
+        
+        public static ALUNO ConsultarById(int idAluno)
+        {
+            return (new DAluno()).ConsultarById(idAluno);
+        }
+
+        public static ALUNO ConsultarByRA(int nuRA)
+        {
+            return (new DAluno()).ConsultarByRA(nuRA);
+        }
+    }
+}
