0bc29ae84f1c1836484f6cedfdd22cbafb3cc81f
contatoaugusto@gmail.com <contatoaugusto@gmail.com@1554cf2e-8db9-679d-0df5-0a48ea2d30d9>
2014-11-17 13:23:33 +0000

    Controle acesso

--- trunk/EC/Paginas/Lancamento.aspx.cs
+++ trunk/EC/Paginas/Lancamento.aspx.cs
@@ -1,4 +1,4 @@
-﻿using System;
+ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -15,6 +15,15 @@ namespace UI.Web.EC.Paginas
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+        
+            // Controle Acesso
+            int[] cargoComAcesso = { 2 };
+            string mensagem = ControleAcesso.verificaAcesso(cargoComAcesso);
+            if (!mensagem.IsNullOrEmpty())
+            {
+                ClientScript.RegisterClientScriptBlock(GetType(), "Alert", "<script>alert('" + mensagem + "');location.replace('../default.aspx')</script>");
+            }
+            
             if (IsPostBack)
                 return;
 
